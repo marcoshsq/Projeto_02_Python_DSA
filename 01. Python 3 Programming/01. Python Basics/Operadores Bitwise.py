4fc2e11e14bf8007334f6478b47c0d489a8e4811
Marcos Henrique Silva Quirino <marcoshsq@gmail.com>
2022-05-29 05:30:04 -0300

    update

--- 01. Python 3 Programming/01. Python Basics/Operadores Bitwise.py
+++ 01. Python 3 Programming/01. Python Basics/Operadores Bitwise.py
@@ -56,44 +56,6 @@ def test_bitwise_operators():
     assert 5 << 2 == 20  # 0b10100
 
 
-
-"""Operadores de comparação
-
-Os operadores de comparação são usados para comparar dois valores.
-"""
-
-
-def test_comparison_operators():
-    """Operadores de comparação"""
-
-    # Igual.
-    number = 5
-    assert number == 5
-
-    # Diferente.
-    number = 5
-    assert number != 3
-
-    # Maior que.
-    number = 5
-    assert number > 3
-
-    # Menor que.
-    number = 5
-    assert number < 8
-
-    # Maior ou Igual que
-    number = 5
-    assert number >= 5
-    assert number >= 4
-
-    # Menor ou Igual que
-    number = 5
-    assert number <= 5
-    assert number <= 6
-
-
-
 """(づ｡◕‿‿◕｡)づ
 
 """
